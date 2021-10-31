595f9e5073356aa866744d5e23d4e81eb91e7c1e
“denis” <denis-kustov@rambler.ru>
2021-11-01 00:00:36 +0300

    2

--- myapp/static/myapp/js/basket.js
+++ myapp/static/myapp/js/basket.js
@@ -2,18 +2,26 @@
 $(document).ready(function () {
     // Перехватываем отправку формы
     $('.BasketForm').submit(function () {
+
+        // Сериализуем данные из формы и имя формы.
+        const dataVAr = $(this).serialize() + '&flag=' + $(this).attr('name')
+
         // Отладочная информация
         console.log("ClickButtonBasket : ")
         console.log($(this).serialize())
         console.log($(this).attr('method'))
         console.log($(this).attr('action'))
+        console.log($(this).attr('name'))
+        console.log(dataVAr)
+
 
         // Отправляем `ajax` запрос
         $.ajax({
             // Тело сообщения
             method: $(this).attr('method'), // Берем метод из формы
             url: $(this).attr('action'), // Берем url из формы
-            data: $(this).serialize(), // Сереализуем данные из формы в json
+            data: dataVAr, // Данные на сервер
+
 
             // Если при отправке возникли ошибки
             error: function (response) {
@@ -29,4 +37,51 @@ $(document).ready(function () {
         // Остановить перезагрузку страницы
         return false;
     });
+
+
 })
+
+
+function getInfoProductIntId(idProduct) {
+    // Получить значения товара по `id`
+    let product = document.getElementById(idProduct);
+    console.log("Товар", product);
+    let infoBottom = product.getElementsByClassName("bottom-info-basket");
+
+    console.log("Информация", infoBottom);
+
+    let price = infoBottom[0].children[0].innerText;
+    let count = infoBottom[0].children[1].innerText;
+    let allPriceProduct = infoBottom[0].children[2].innerText;
+
+    console.log("цена", price);
+    console.log("штук", count);
+    console.log("сумма", allPriceProduct);
+}
+
+
+function deleteProduct(idProduct) {
+    // Удаление товара по `id`
+
+    let product = document.getElementById(idProduct);
+    console.log("Товар", product);
+
+    let infoBottom = product.getElementsByClassName("bottom-info-basket");
+    console.log("Информация", infoBottom);
+
+    infoBottom[0].children[1].innerText -= 1;
+    infoBottom[0].children[2].innerText = infoBottom[0].children[0].innerText * infoBottom[0].children[1].innerText
+
+    if (infoBottom[0].children[1].innerText === '0') {
+        console.log(`Удаление элемента ${idProduct}`);
+        product.remove()
+    }
+
+    let price = infoBottom[0].children[0].innerText;
+    let count = infoBottom[0].children[1].innerText;
+    let allPriceProduct = infoBottom[0].children[2].innerText;
+
+    console.log("цена", price);
+    console.log("штук", count);
+    console.log("сумма", allPriceProduct);
+}
--- myapp/views.py
+++ myapp/views.py
@@ -1,14 +1,13 @@
 from pprint import pprint
-from typing import Any
+from typing import Any, Union
 
+import pydantic
 from django.core.handlers.wsgi import WSGIRequest
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
-from django.urls import reverse_lazy
+from django.shortcuts import render
 from django.views import View
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView
 
-from myapp.forms import ProductForm
 from myapp.models import Product
 
 
@@ -82,10 +81,47 @@ class ProductDetailView(DetailView):
 class BasketView(View):
     template_name = "myapp/basket.html"  # Путь к шаблону `html`
     http_method_names = ["get", "post", ]  # Список методов HTTP, которые обрабатывает класс.
-
-    basket_name = "basket_id"
-
     model = Product  # Какую модель используем
+    basket_name: str = "product_basket"  # Ключ для словаря в сессии
+
+    class Basket(pydantic.BaseModel):
+        """
+        Структура корзины c товарами
+        """
+        AllProduct: int
+        content: dict[str, int]
+
+        @classmethod
+        def session_parse_raw(cls, *args, **kwargs):
+            """
+            Читаем данные из сессии, если они корректны, то десериализуем их,
+            а если они не корректные, то создаем пустой объект.
+            """
+            try:
+                return cls.parse_raw(*args, **kwargs)
+            except pydantic.error_wrappers.ValidationError:
+                return cls(AllProduct=0, content={})
+
+        def addProductInBasket(self, _id_product: str):
+            """
+            Добавляем товар в корзину
+            """
+            product: Union[None, int] = self.content.get(_id_product, None)
+            if product:
+                # Если один и тот же товар добавляют в корзину, то добавляем в количество.
+                self.content[_id_product] += 1
+            else:
+                self.content[_id_product] = 1  # Добавляем id товара в массив.
+            self.AllProduct += 1  # Прибавляем один товар в общее число товаров.
+
+        def deleteProductInBasket(self, _id_product: str):
+            product: Union[None, int] = self.content.get(_id_product, None)
+            if product:
+                # Если один и тот же товар добавляют в корзину, то добавляем в количество.
+                self.content[_id_product] -= 1
+                if self.content[_id_product] == 0:
+                    self.content.pop(_id_product)
+                self.AllProduct -= 1  # Прибавляем один товар в общее число товаров.
 
     def get(self, request: WSGIRequest, *args, **kwargs):
         """
@@ -101,60 +137,95 @@ class BasketView(View):
     def post(self, request: WSGIRequest, *args, **kwargs):
         """
         В методе обрабатывается POST запрос
-        request.method == "POST"
+        request.method == "POST".
         """
-        pprint(request.POST)
-        request.session[BasketView.basket_name] = request.POST.get("id-product")
-        return HttpResponse(f"{request.POST}",status=200)
+
+        id_product: str = request.POST.get("id-product")  # Получаем  id товара.
+        user_session: str = request.session.get(self.basket_name)  # Получаем корзину из сессии.
+        basket_json = self.Basket.session_parse_raw(user_session)  # Десериализуем корзину.
+
+        # Флаг, который определяет добавить или удалить товар из корзины.
+        flag_product = request.POST.get('flag')
+
+        """
+        Значение для флагов берутся из имени форм. 
+        - `button-add-basket.html`
+        - `button-delete-basket.html`
+        """
+        if flag_product == "AddProduct":
+            """
+            Если нужно добавить товар в корзину.
+            """
+            basket_json.addProductInBasket(id_product)  # Добавляем товар в корзину.
+
+        elif flag_product == "DeleteProduct":
+            """
+            Если нужно удалить товар в корзины.
+            """
+            basket_json.deleteProductInBasket(id_product)  # Удалить товар из корзины.
+
+        pprint(basket_json)
+        request.session[self.basket_name] = basket_json.json()  # Сохраняем массив в сессию пользователя.
+        return HttpResponse(basket_json.AllProduct, status=200)  # Отправляем количество товаров в корзине.
 
     def get_context_data(self, **kwargs) -> dict[str, Any]:
         """
-        Сформировать контекст для шаблона `html`
+        Сформировать контекст для шаблона `html`.
         """
-        tmp = self.request.session.get(BasketView.basket_name)
-        res = None
-        if tmp:
-            res = self.model.objects.get(pk=tmp)
+        tmp = self.request.session.get(self.basket_name)
+        basket_json = self.Basket.session_parse_raw(tmp)
+
+        list_key_basket_json = list(basket_json.content.keys())
+        res_db = self.model.objects.filter(id__in=list_key_basket_json).order_by("-price")
+        allPrice = 0
+        all_product = []
+        for _product in res_db:
+            allPrice += _product.price * basket_json.content[str(_product.id)]
+            all_product.append((_product, basket_json.content[str(_product.id)]))
+
+        pprint(all_product)
+        pprint(allPrice)
 
         context = {
-            "basket": res
+            "basket": all_product,
+            "allPrice": allPrice,
         }
         return context
 
 
-def form_test(request: WSGIRequest):
-    form = ProductForm.save_from_form(request)
-
-    context = {
-        "form": form[1]
-    }
-    print(form)
-    return render(request,
-                  template_name="myapp/form_test.html",
-                  context=context, )
-
-
-def home_fun(request: WSGIRequest):
-    context = {
-    }
-    return render(request,
-                  template_name="myapp/chaild.html",
-                  context=context, )
-
-
-def test_db(request: WSGIRequest):
-    x = Product.objects.all().order_by("-star").values("rating", "id")
-    y = x.values("id")
-    print(y)
-
-    context = {
-        "MyDataBase": x,
-        "MyDataBase2": y,
-
-    }
-    return render(request,
-                  template_name="myapp/test_db.html",
-                  context=context, )
+# def form_test(request: WSGIRequest):
+#     form = ProductForm.save_from_form(request)
+#
+#     context = {
+#         "form": form[1]
+#     }
+#     print(form)
+#     return render(request,
+#                   template_name="myapp/form_test.html",
+#                   context=context, )
+#
+#
+# def home_fun(request: WSGIRequest):
+#     context = {
+#     }
+#     return render(request,
+#                   template_name="myapp/chaild.html",
+#                   context=context, )
+#
+#
+# def test_db(request: WSGIRequest):
+#     x = Product.objects.all().order_by("-star").values("rating", "id")
+#     y = x.values("id")
+#     print(y)
+#
+#     context = {
+#         "MyDataBase": x,
+#         "MyDataBase2": y,
+#
+#     }
+#     return render(request,
+#                   template_name="myapp/test_db.html",
+#                   context=context, )
 
 
 class index_main(View):
